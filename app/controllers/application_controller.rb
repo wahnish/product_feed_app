8fa28ae3bc1f58bbaf4b8b4303443d3e55316fee
Todd Wahnish <t.wahnish@gmail.com>
2013-03-10 13:49:51 -0400

    fix FB Omniauth/Devise log out and log in issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-# Facebook Authentication (#360)
-# private
-
-# 	def current_user
-# 	  @current_user ||= User.find(session[:user_id]) if session[:user_id]
-# 	end
-# 	helper_method :current_user
-
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,15 +14,4 @@ class SessionsController < ApplicationController
 	  end
 	end
 
-#For Facebook login
-	# def create
-	#     user = User.from_omniauth(env["omniauth.auth"])
-	#     session[:user_id] = user.id
-	#     redirect_to root_url
- #  	end
-
-	# def destroy
-	#   session[:user_id] = nil
-	#   redirect_to root_url, :notice => "Logged out!"
-	# end
 end
